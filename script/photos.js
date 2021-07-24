998ab0969d690f957167fbbea1ec381f21ef63f8
kemal <yavuz.imer133@gmail.com>
2021-07-24 20:38:46 +0300

    photos kısmına veriler çekildi

---
+++ script/photos.js
@@ -0,0 +1,14 @@
+function fetchphotos() {
+    let template = ""
+    fetch("https://api.unsplash.com/users/yavuzim/photos?client_id=QPwn6dddEmZllOFESijqiqBox6mMlRWJ_mzIIkWkhRg").then(res => res.json()).then(data => {
+        data.forEach(image => {
+            template += ` <div class="flex-item">
+            <div class="item-inner">
+                <img src="${image.urls.small}" alt="" class="image">
+            </div>
+         </div>`
+        });
+        document.querySelector("#gallery").innerHTML = template;
+    })
+}
+fetchphotos();
